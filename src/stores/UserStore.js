180a53eca31154b94a85db52bc565f9c91d80dfa
Uhliber <olvrcp@gmail.com>
2023-10-01 18:57:36 +0800

    change user

--- src/stores/UserStore.js
+++ src/stores/UserStore.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 
 export const useUserStore = defineStore('UserStore', {
   state: () => ({
-    user: 'Ben Hong'
+    user: 'Olie Cape'
   }),
   getters: {
     firstName() {
